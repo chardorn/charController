c7c41e5f98f425fa992a9a5c7909e197f2830180
Shared Car Acct <otternes@cs.unc.edu>
2020-01-24 14:35:44 -0500

    trying threading

--- joyTeleop.py
+++ joyTeleop.py
@@ -5,28 +5,38 @@ from std_msgs.msg import Bool
 import time
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Joy
+import threading
 
 msg = drive_params()
 msg.angle = 0.5
 global_scans = LaserScan()
 max_index = 540
+autonomous = 0
 
 
 def joy_callback(data):
+    print("Call back!")
+    global autonomous
     if data.buttons[1] == 1:
-        t1.start()
-        t2.start()
+        autonomous = 1
+        if not t1.isAlive():
+            t1.start()
+        #t2.start()
+    #if data.buttons[1] == 0:
+     #   t1.join()
     else:
+        autonomous = 0
+        
         manual(data)
 
 def manual(data):
-    if data.axes[1] > 0:
+    if data.axes[5] > 0:
         print("forward")
         if data.buttons[5] == 1.0:
             msg.velocity = 0.2
         else:
             msg.velocity = 0.1
-    elif data.axes[1] < 0:
+    elif data.axes[5] < 0:
         print("backward")
         if data.buttons[5] == 1.0:
             msg.velocity = 0.2
@@ -39,39 +49,45 @@ def manual(data):
     #msg.velocity = 0.05
     DriveParamPublisher.publish(msg)
 
-def autonomous():
-    print("Autonomous!")
-    global global_scans
-    print(global_scans)
-    distances = global_scans.ranges
-    num_scans = len(distances)
-    if num_scans == 0:
-        return
-    #start_index = int(num_scans * 0.25)
-    #end_index = int(num_scans * 0.75)
-    #index = start_index
-    index = 0
-    max_distance = 0.0
-    max_index = num_scans / 2
-    while(index < num_scans - 1):
-        index = index + 1
-        if distances[index] > max_distance:
-            max_distance = distances[index]
-            max_index = index
+def autonomous(data):
+    print("Called Autonomous")
+    global autonomous
+    while(autonomous == 1):
+        print("Autonomous!")
+        global global_scans
+        print(global_scans)
+        distances = global_scans.ranges
+        num_scans = len(distances)
+        if num_scans == 0:
+          return
+        #start_index = int(num_scans * 0.25)
+        #end_index = int(num_scans * 0.75)
+        #index = start_index
+        index = 0
+        max_distance = 0.0
+        max_index = num_scans / 2
+        while(index < num_scans - 1):
+            index = index + 1
+            if distances[index] > max_distance:
+                max_distance = distances[index]
+                max_index = index
+            else:
+                continue
+        print("Max Index: " + str(max_index))
+        #left is 0 and right is 1
+        #sory, this is stuipid
+        float_index = 0.1 + index - 0.1
+        servo_value = index / num_scans
+        print("Servo Value: " + str(servo_value))
+        msg.angle = servo_value
+        if distances[num_scans / 2] < 0.5:
+            msg.velocity = 0.0
         else:
-            continue
-    print("Max Index: " + str(max_index))
-    #left is 0 and right is 1
-    #sory, this is stuipid
-    float_index = 0.1 + index - 0.1
-    servo_value = index / num_scans
-    print("Servo Value: " + str(servo_value))
-    msg.angle = servo_value
-    if distances[num_scans / 2] < 0.5:
-        msg.velocity = 0.0
-    else:
-        msg.velocity = 0.05
-    DriveParamPublisher.publish(msg)
+            msg.velocity = 0.05
+        DriveParamPublisher.publish(msg)
+
+    if t1.isAlive():
+        t1.join()
 
 def lidar_callback(scans):
     global global_scans
@@ -105,8 +121,9 @@ def backUp():
     msg.velocity = 0
     DriveParamPublisher.publish(msg)
 
+print("Setting up... ")
 t1 = threading.Thread(target=autonomous, args=(10,))
-t2 = threading.Thread(target= , args=(10,))
+#t2 = threading.Thread(target= , args=(10,))
 
 DriveParamPublisher = rospy.Publisher("drive_parameters", drive_params, queue_size=10)
 EStopPublisher = rospy.Publisher("eStop", Bool, queue_size=10)
@@ -115,9 +132,5 @@ rospy.Subscriber('joy', Joy, joy_callback)
 rospy.init_node("JoyControl")
 rospy.spin()
 
-
-
-
-
 t1.join()
-t2.join()
+#t2.join()
